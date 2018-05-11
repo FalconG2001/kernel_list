7fc4aa0c061ef48d08c0677c555c2bda8bad88ad
zhenyu <zhenyu_tien@gemteks.com>
2018-05-11 14:17:28 +0800

    修改"link_list.c"、新增read_str.c讀取檔案的字串、修改Makefile link_list.c: 刪除read_file 變更func add_entry prototype, 增加malloc struct mylist 變更func free_list使用的macro 將每個func由void改為int main新增讀取字串的操作
    read_str.c
    新增func read_str, 將原本read_file做修改, 讀取檔案裡的字串並回傳
    
    Makefile:
    修改為將每個.c檔分開編譯

--- Makefile
+++ Makefile
@@ -1,8 +1,19 @@
 SHELL=/bin/bash
 EXEC = list_sort
+DEPS = list.h mylist.h
+OBJ = link_list.o read_str.o
 
-$(EXEC): link_list.c list.h
-	gcc -o $@ $<
+all: $(EXEC)
+
+
+$(EXEC): $(OBJ)
+	gcc -o $@ $^
+
+link_list.o: link_list.c $(DEPS)
+	gcc -c -o $@ $<
+
+read_str.o: read_str.c
+	gcc -c -o $@ $<
 
 question.txt:
 	for i in {1..100}; do echo $$RANDOM; done > question.txt
@@ -15,3 +26,6 @@ output.txt: question.txt $(EXEC)
 
 test: answer.txt output.txt
 	diff -q answer.txt output.txt
+
+clean:
+	rm -f *.o question.txt answer.txt output.txt $(EXEC)
--- link_list.c
+++ link_list.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include "list.h"
 
+extern int read_str(FILE *fp, char **str);
+
 struct mylist{
 	char *str;
 	int str_length;
@@ -12,111 +14,74 @@ struct mylist{
 };
 
 // print each element
-void print_list(struct list_head *head) {
-    struct list_head *listptr;
+int print_list(struct list_head *head) {
     struct mylist *entry;
-    
     list_for_each_entry(entry, head, list) {
         printf("%s\n", entry->str);
     }
+    return 0;
 }
 
-
 // add entry to list
-void add_entry(struct list_head *head, struct mylist *new) {
+int add_entry(struct list_head *head, char *str) {
+	//create entry
+	struct mylist *new = malloc(sizeof(struct mylist));
+	if(new == NULL) {
+		printf("malloc failed\n");
+		exit(1);
+	}
+	new->str_length = strlen(str);
+	new->str = str;
+	
 	// add new entry directly if list is empty
 	if(list_empty(head)) {
 		list_add(&new->list, head);
-		return;
+		return 0;
 	}
 	
 	// compare each element
-	struct list_head *listptr;
 	struct mylist *entry;
-
-	list_for_each(listptr, head) {
-		entry = list_entry(listptr, struct mylist, list);
+	list_for_each_entry(entry, head, list) {
 		// add new entry before current entry if length of current entry is larger than length of new entry
 		if(new->str_length < entry->str_length) {
 			list_add_tail(&new->list, &entry->list);
-			return;
+			return 0;
 		} 
 		// do string compare if two strings have same length
 		else if(entry->str_length == new->str_length) {
 			if(strcmp(new->str, entry->str) <= 0) {
 				list_add_tail(&new->list, &entry->list);
-				return;
+				return 0;
 			}
 		}
 	}
 	// add new entry to the last since it is the latgest number
 	list_add_tail(&new->list, head);
+	return 0;
 }
 
-// read file content
-void read_file(struct list_head *head, FILE *fp) {
-	int buf_length = 2, str_length = 0;
-	char c, *cp = (char*) malloc(buf_length * sizeof(char));
-	
-	while(c = fgetc(fp)) {
-		if(c == EOF) {
-			free(cp);
-			break;
-		}
-		
-		
-		if(c == '\n') {
-			cp[str_length] = '\0';
-			// add element to list
-			struct mylist *new = malloc(sizeof(struct mylist));
-			new->str_length = str_length;
-			new->str = cp;
-			add_entry(head, new);
-			
-			// initialize
-			buf_length = 2;
-			str_length = 0;
-			cp = (char*) malloc(buf_length * sizeof(char));
-			continue;
-		}
-		
-		cp[str_length] = c;
-		str_length++;
-		
-		// double the buf size if length of string is bigger than buf
-		if(buf_length == str_length){
-			buf_length *= 2;
-			cp = (char*) realloc(cp, buf_length * sizeof(char));
-		}
-	}
-}
-
-void free_list(struct list_head *head)
+int free_list(struct list_head *head)
 {
-	struct list_head *listptr;
-    struct mylist *entry;
-    
-    list_for_each(listptr, head) {
-        entry = list_entry(listptr, struct mylist, list);
+    struct mylist *entry, *tmp_entry;
+    list_for_each_entry_safe(entry, tmp_entry, head, list) {
         list_del(&entry->list);
         free(entry->str);
         free(entry);
     }
+    return 0;
 }
 
 int main()
 {
-	LIST_HEAD(head); //create head and initialize
+	LIST_HEAD(head); // create head and initialize
 	
+	// read string from file and add to list
 	FILE *fp = stdin;
-	/*
-	if(fp = fopen("question.txt", "r")) {
-		printf("file is opened\n");
-		read_file(&head, fp);
+	char *str = NULL;	
+	while(read_str(fp, &str)) {
+		add_entry(&head, str);
 	}
-	*/
-	read_file(&head, fp);
-	
+
 	print_list(&head);
 	free_list(&head);
 	return 0;
